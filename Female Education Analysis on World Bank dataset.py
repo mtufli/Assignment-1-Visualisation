9d58bf965a7484f58bd60ec3868904fb4d541fa3
mtufli <126280247+mtufli@users.noreply.github.com>
2023-02-25 21:21:33 +0000

    Update Female Education Analysis on World Bank dataset.py

--- Female Education Analysis on World Bank dataset.py
+++ Female Education Analysis on World Bank dataset.py
@@ -48,3 +48,38 @@ df = df[df["Country Name"].isin(country_names)]
 index = df[df["Country Name"] == "World"].index
 df.drop(index, axis=0, inplace=True)
 df.reset_index(drop=True, inplace=True)
+
+# # Visualizations
+
+# In[4]:
+
+
+useless_cols = ["Country Code", "Indicator Name", "Indicator Code"]
+df_edu = df.drop(useless_cols, axis=1).set_index("Country Name").iloc[:, :-2]
+df_edu.iloc[:, 30:].mean().plot(kind="line", figsize=(14, 5))
+plt.title("Average Female Primary Education Completion Rate in whole World", fontsize=18)
+plt.show()
+
+
+# In[5]:
+
+
+temp = df_edu.iloc[:, 30:].mean(axis=1).sort_values(ascending=False)
+temp = temp[temp > 0][9:29]
+plt.figure(figsize=(16, 6))
+sns.barplot(temp.index, temp.values)
+plt.title("Top 20 Countries with Highest Female Primary Education Completion Rate", fontsize=18)
+plt.xticks(rotation=90)
+plt.show()
+
+
+# In[42]:
+
+
+temp = df_edu.mean()
+temp = temp[temp>0]
+
+plt.figure(figsize=(10,8))
+plt.pie(temp.values, labels=temp.index)
+plt.title("Education Completion rate per Year of whole world")
+plt.show()
